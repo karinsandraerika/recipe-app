488b4291fa46dfd6e0b5f3e848dfbbc8ea978e68
karinsandraerika <65858154+karinsandraerika@users.noreply.github.com>
2022-12-06 11:56:16 +0100

    Activities added

---
+++ app/src/main/java/com/example/recipeapp/RecipesListActivity.java
@@ -0,0 +1,14 @@
+package com.example.recipeapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RecipesListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipes_list);
+    }
+}
