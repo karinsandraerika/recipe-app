5e46850d45c499104a6754736809c5da66737c67
lisabjo <115546164+lisabjo@users.noreply.github.com>
2022-12-07 10:42:59 +0100

    Updated Recipe

--- app/src/main/java/com/example/recipeapp/Recipe.java
+++ app/src/main/java/com/example/recipeapp/Recipe.java
@@ -1,39 +1,69 @@
 package com.example.recipeapp;
 
 public class Recipe {
+    private int id;
     private Category category;
     private String name;
-    //private String[] ingredients; //kanske bara en string istället för en array
-    private String instructions; //just nu är detta både ingredienslista och instruktioner
+    private String ingredients;
+    private String instructions;
 
-    public Recipe(Category category, String name, String instructions) {
+    public Recipe(){
+        id = 0;
+        category = Category.RECIPEFORDISASTER; //Vi behöver nog en "övrigt"-kategori, som default
+        name = ingredients = instructions = "";
+    }
+
+    public Recipe(int id, Category category, String name, String ingredients, String instructions) {
         //lägga till validering här eller i annan klass innan objekt skapas
+        this.id = id;
         this.category = category;
         this.name = name;
+        this.ingredients = ingredients;
         this.instructions = instructions;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Recipe setId(int id) {
+        this.id = id;
+        return this;
     }
 
     public Category getCategory() {
         return category;
     }
 
-    public void setCategory(Category category) {
+    public Recipe setCategory(Category category) {
         this.category = category;
+        return this;
     }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
+    public Recipe setName(String name) {
         this.name = name;
+        return this;
+    }
+
+    public String getIngredients() {
+        return ingredients;
+    }
+
+    public Recipe setIngredients(String ingredients) {
+        this.ingredients = ingredients;
+        return this;
     }
 
     public String getInstructions() {
         return instructions;
     }
 
-    public void setInstructions(String instructions) {
+    public Recipe setInstructions(String instructions) {
         this.instructions = instructions;
+        return this;
     }
 }
