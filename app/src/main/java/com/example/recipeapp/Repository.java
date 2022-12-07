bc88eb70320288294106483c14e2a92cbcfd5422
Edvin Hermansson <edvin.hermanson@gmail.com>
2022-12-07 10:29:10 +0100

    Added StubRepo

--- app/src/main/java/com/example/recipeapp/Repository.java
+++ app/src/main/java/com/example/recipeapp/Repository.java
@@ -1,9 +1,11 @@
 package com.example.recipeapp;
 
+import java.util.ArrayList;
+
 public interface Repository {
 
     // Get all recipes that belong to a certain category.
-    RecipeListItem[] filterByCategory(Category category);
+    ArrayList<RecipeListItem> filterByCategory(Category category);
     // Get a recipe by the id.
     Recipe findRecipeById(int id);
     // Save a recipe to the database. Returns -1 if unsuccessful else returns the id.
