8524a02e3a822404225aa9dafb81b8100096d1fd
Edvin Hermansson <edvin.hermanson@gmail.com>
2022-12-08 21:28:43 +0100

    Fixed index dependency

--- app/src/main/java/com/example/recipeapp/Repository.java
+++ app/src/main/java/com/example/recipeapp/Repository.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 public interface Repository {
 
     // Get all recipes that belong to a certain category.
-    ArrayList<RecipeListItem> filterByCategory(Category category);
+    ArrayList<Recipe> filterByCategory(Category category);
     // Get a recipe by the id.
     Recipe findRecipeById(int id);
     // Save a recipe to the database. Returns true if successful else returns false.
