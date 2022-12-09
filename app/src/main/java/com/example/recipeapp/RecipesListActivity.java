9cab45a28404b69e733f2e2ad35766068120e82f
Mariam Mirström <maryammirzaieu@yahoo.com>
2022-12-09 10:19:55 +0100

    Navigation

--- app/src/main/java/com/example/recipeapp/RecipesListActivity.java
+++ app/src/main/java/com/example/recipeapp/RecipesListActivity.java
@@ -2,13 +2,43 @@ package com.example.recipeapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class RecipesListActivity extends AppCompatActivity {
+
+
+    ListView l;
+    String tutorials[]
+            = { "....", ".....",
+            "....", "...",
+            "....", ".....",
+            "....", ".....",
+            "....." };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_recipes_list);
+        setContentView(R.layout.activity_recipes_list_1);
+
+        l = findViewById(R.id.list);
+        ArrayAdapter<String> arr;
+        arr
+                = new ArrayAdapter<String>(
+                this,
+              androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,
+                tutorials);
+        l.setAdapter(arr);
+        l.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(getApplicationContext(), ReadRecipeActivity.class);
+                startActivity(intent);
+              }
+        });
     }
 }
